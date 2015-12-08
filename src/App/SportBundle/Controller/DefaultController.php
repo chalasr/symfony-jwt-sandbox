4dc4741c7fad4f2f24e9d60c55bd82f84c4d63ba
chalasr <robin.chalas@epitech.eu>
2015-12-08 22:46:54 +0100

    redirect / on admin

--- src/App/SportBundle/Controller/DefaultController.php
+++ src/App/SportBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppSportBundle:Default:index.html.twig');
+        return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
     }
 }
