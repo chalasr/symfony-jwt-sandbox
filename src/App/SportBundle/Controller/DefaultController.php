44726a47324c3a5c8b238fdae95c0277a5b6686b
chalasr <robin.chalas@epitech.eu>
2015-12-10 11:38:40 +0100

    begin test call

--- src/App/SportBundle/Controller/DefaultController.php
+++ src/App/SportBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\SportBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -11,5 +12,10 @@ class DefaultController extends Controller
     {
         // return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
         return new Response('API HOME');
+    }
+
+    public function callTestAction()
+    {
+        return new JsonResponse(['content' => 'hello']);
     }
 }
