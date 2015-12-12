4d27c3b22d33494d98cd6f4f1fd7b63fcf4f9f86
chalasr <robin.chalas@epitech.eu>
2015-12-12 01:14:50 +0100

    first secured call api as demo

--- src/App/SportBundle/Controller/DefaultController.php
+++ src/App/SportBundle/Controller/DefaultController.php
@@ -4,12 +4,36 @@ namespace App\SportBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         // return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
-        return new Response('API HOME');
+        return $this->render('AppUserBundle:Default:list.html.twig');
+    }
+
+    public function getUsersListAction()
+    {
+        $em = $this->getEntityManager();
+        $repo = $em->getRepository('AppUserBundle:User');
+        $query = $repo->createQuerybuilder('u')
+        ->select('u.username', 'u.email')
+        ->getQuery();
+        // $toJson = [];
+        // foreach ($users as $u) {
+        //     $toJson[  ] = [
+        //         'username' => $u->getUsername(),
+        //         'email'    => $u->getEmail(),
+        //     ];
+        // }
+
+        return new JsonResponse($query->getResult());
+    }
+
+    protected function getEntityManager()
+    {
+        return $this->getDoctrine()->getEntityManager();
     }
 }
