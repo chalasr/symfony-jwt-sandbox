46394481788441a5a5b57fc1398fb805d5973362
chalasr <robin.chalas@epitech.eu>
2016-01-11 18:13:21 +0100

    Added icon upload in Sport Admin + improve base system

--- src/App/AdminBundle/Admin/SportAdmin.php
+++ src/App/AdminBundle/Admin/SportAdmin.php
@@ -5,6 +5,7 @@ namespace App\AdminBundle\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\Routecollection;
 
 /**
  * Sport admin class.
@@ -18,8 +19,30 @@ class SportAdmin extends AbstractAdmin
     /**
      * {@inheritdoc}
      */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->add('show_icon', 'icon/{name}', [], [], ['expose' => true]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $iconFieldOptions =  array(
+            'required'   => false,
+            'data_class' => null,
+            'label'      => 'Icône',
+        );
+
+        if ($this->getSubject()->getId()) {
+            $subject = $this->getSubject();
+            if ($subject->getIcon()) {
+                $path = $this->generateUrl('show_icon', ['name' => $subject->getIcon()]);
+                $iconFieldOptions['help'] = sprintf('<img style="max-width: 100px;" src="%s"/>', $path);
+            }
+        }
+
         $formMapper
             ->add('name', null, array(
                 'label' => 'Nom',
@@ -30,7 +53,9 @@ class SportAdmin extends AbstractAdmin
             ->add('isActive', null, array(
                 'label' => 'Actif',
                 'required' => false,
-            ));
+            ))
+            ->add('file', 'file', $iconFieldOptions)
+        ;
     }
 
     /**
@@ -44,6 +69,9 @@ class SportAdmin extends AbstractAdmin
             ))
             ->add('name', null, array(
                 'label' => 'Nom',
+            ))
+            ->add('categories', null, array(
+                'label' => 'Categories',
             ))
             ->add('isActive', null, array(
                 'label' => 'Actif',
@@ -75,5 +103,32 @@ class SportAdmin extends AbstractAdmin
                 ),
             ])
         ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prePersist($created)
+    {
+        $uploadPath = $this->locate('@AppSportBundle/Resources/public/icons');
+        if ($created->getFile()) {
+            $created->uploadIcon($uploadPath);
+        }
+
+        return $created;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function preUpdate($updated)
+    {
+        $uploadPath = $this->locate('@AppSportBundle/Resources/public/icons');
+
+        if ($updated->getFile()) {
+            $updated->uploadIcon($uploadPath);
+        }
+
+        return $updated;
     }
 }
