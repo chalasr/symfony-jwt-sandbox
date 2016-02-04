433df151c0b1691e2628d12a81899cfa3773f524
chalasr <robin.chalas@epitech.eu>
2016-02-04 11:23:46 +0100

    Fixed spelling on BO labels list

--- src/App/AdminBundle/Admin/User/Information/CoachInformationAdmin.php
+++ src/App/AdminBundle/Admin/User/Information/CoachInformationAdmin.php
@@ -16,13 +16,13 @@ class CoachInformationAdmin extends Admin
                 'dp_language' => 'fr',
             ))
             ->add('insuranceCompanyName', null, array(
-                'label' => 'Nom de la compagnie fournissant la police d\'insurance',
+                'label' => 'Nom de la compagnie d\'assurance',
             ))
             ->add('insurancePolicyNumber', null, array(
-                'label' => 'Numéro de police d\'insurance',
+                'label' => 'Numéro de police d\'assurance',
             ))
             ->add('insurancePolicyExpirationDate', 'sonata_type_date_picker', array(
-                'label'       => 'Date d\'expiration de la police d\'insurance',
+                'label'       => 'Date d\'expiration de la police d\'assurance',
                 'format'      => 'dd/MM/yyyy',
                 'dp_language' => 'fr',
             ))
